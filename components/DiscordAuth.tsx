aeba0c08e550325937c9689dcac9bdc78b669de8
maat <matt@mattchan.xyz>
2024-02-07 14:09:02 -0500

    See if adding /auth/callback works

--- components/DiscordAuth.tsx
+++ components/DiscordAuth.tsx
@@ -39,8 +39,8 @@ export default async function DiscordAuth() {
     "use server";
     const getURL = () => {
       let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        `${process?.env?.NEXT_PUBLIC_SITE_URL}/auth/callback` ?? // Set this to your site URL in production env.
+        `${process?.env?.NEXT_PUBLIC_VERCEL_URL}/auth/callback` ?? // Automatically set by Vercel.
         "http://localhost:3000/auth/callback";
       // Make sure to include `https://` when not localhost.
       url = url.includes("http") ? url : `https://${url}`;
