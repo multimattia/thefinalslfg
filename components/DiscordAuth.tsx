d1c991edb1bcfea0d1b0d3e35f413e371d196cfb
maat <matt@mattchan.xyz>
2024-02-07 14:41:59 -0500

    Resolve merge conflict

--- components/DiscordAuth.tsx
+++ components/DiscordAuth.tsx
@@ -24,8 +24,7 @@ export default async function DiscordAuth() {
     "use server";
     const getURL = () => {
       let url =
-        `${process?.env?.NEXT_PUBLIC_SITE_URL}/auth/callback` ?? // Set this to your site URL in production env.
-        `${process?.env?.NEXT_PUBLIC_VERCEL_URL}/auth/callback` ?? // Automatically set by Vercel.
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
         "http://localhost:3000/auth/callback";
       // Make sure to include `https://` when not localhost.
       url = url.includes("http") ? url : `https://${url}`;
