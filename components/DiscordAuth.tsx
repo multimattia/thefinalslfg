e5785591e5877f09efa7fecc3d81593d8daf3445
maat <matt@mattchan.xyz>
2024-02-07 14:07:01 -0500

    Add env variable for production only

--- components/DiscordAuth.tsx
+++ components/DiscordAuth.tsx
@@ -39,7 +39,7 @@ export default async function DiscordAuth() {
     "use server";
     const getURL = () => {
       let url =
-        // process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
         process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
         "http://localhost:3000/auth/callback";
       // Make sure to include `https://` when not localhost.
