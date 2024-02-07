b93ac6aff4e350929e8e909a4303bbeac576b86c
maat <matt@mattchan.xyz>
2024-02-07 13:58:44 -0500

    Add vercel env for auth redirect url (hopefully correctly this time)

--- components/DiscordAuth.tsx
+++ components/DiscordAuth.tsx
@@ -37,13 +37,24 @@ export default async function DiscordAuth() {
 
   const signIn = async () => {
     "use server";
+    const getURL = () => {
+      let url =
+        // process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/auth/callback";
+      // Make sure to include `https://` when not localhost.
+      url = url.includes("http") ? url : `https://${url}`;
+      // Make sure to include a trailing `/`.
+      url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+      return url;
+    };
 
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "discord",
       options: {
-        redirectTo: authCallbackURL,
+        redirectTo: getURL(),
       },
     });
     if (error) {
