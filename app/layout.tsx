d508e1ffa3eb6bca72bfa0a9855fae73f3ca495a
maat <matt@mattchan.xyz>
2024-02-07 13:23:54 -0500

    Add defaultURL to HTML for debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <main className="flex min-h-screen flex-col items-center">
           {children}
         </main>
+        <p>{defaultUrl}</p>
       </body>
     </html>
   );
