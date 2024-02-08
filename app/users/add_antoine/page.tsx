e1a6d3bab942114c2d8cbe6b2fa12cfbf95d3536
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-02-08 16:24:20 -0500

    added uuid to form submission

--- app/users/add_antoine/page.tsx
+++ app/users/add_antoine/page.tsx
@@ -1,13 +1,19 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default function Page() {
   // function to be called when form is submitted
   async function addUser(formData: FormData) {
     "use server";
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
     const rawFormData = {
       discordName: formData.get("discordName"),
       embarkId: formData.get("embarkId"),
@@ -19,13 +25,15 @@ export default async function Page() {
       discord_name: rawFormData.discordName,
       embark_id: rawFormData.embarkId,
       rank: rawFormData.playerRank,
+      user_id: user?.id,
     });
     console.log(error);
+    return redirect("/users");
   }
 
   return (
     <>
-      <form action={addUser} className="flex inline-flex flex-auto flex-col">
+      <form action={addUser} className="inline-flex flex-auto flex-col">
         <label htmlFor="discordName">Discord name</label>
         <input name="discordName" type="text" className="border-2" />
 
