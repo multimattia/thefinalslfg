92d4123af3fa4a8f932034d1d928108f57e30c9e
maat <matt@mattchan.xyz>
2024-02-08 13:17:51 -0500

    Install shadcn, move tailwindconfig from .js -> .ts

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,16 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
   const { data: users } = await supabase.from("users").select();
 
-  return <pre>{JSON.stringify(users, null, 2)}</pre>;
+  return (
+    <>
+      <Button />
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+    </>
+  );
 }
