b019f7ae9a58ff3d807b2e07251179d5c5153cc8
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-02-08 12:59:39 -0500

    cleanup

--- app/users/add_antoine/page.tsx
+++ app/users/add_antoine/page.tsx
@@ -3,8 +3,8 @@ import { cookies } from "next/headers";
 
 
 export default async function Page() {
-  // supabase variables
 
+  // function to be called when form is submitted
   async function addUser(formData: FormData) {
     "use server"
     const cookieStore = cookies();
@@ -14,8 +14,8 @@ export default async function Page() {
       embarkId: formData.get('embarkId'),
       playerRank: formData.get('playerRank'),
     }
-    console.log(rawFormData)
 
+    // insert form data into supabase
     const { error } = await supabase.from("users").insert({
       discord_name: rawFormData.discordName, embark_id: rawFormData.embarkId, rank: rawFormData.playerRank
       });
@@ -25,18 +25,17 @@ export default async function Page() {
   return (
     <>
       <form action={addUser} className="flex flex-col">
-                <label htmlFor="discordName">Discord name</label>
-                <input name="discordName" type="text" className="bg-gray-800"/>
+          <label htmlFor="discordName">Discord name</label>
+          <input name="discordName" type="text" className="bg-gray-800"/>
 
-                <label htmlFor="embarkId">Embark ID</label>
-                <input name="embarkId" type="text" className="bg-gray-800"/>
+          <label htmlFor="embarkId">Embark ID</label>
+          <input name="embarkId" type="text" className="bg-gray-800"/>
 
-                <label htmlFor="playerRank">Player rank</label>
-                <input name="playerRank" type="text" className="bg-gray-800"/>
+          <label htmlFor="playerRank">Player rank</label>
+          <input name="playerRank" type="text" className="bg-gray-800"/>
 
-                <button type="submit">add</button>
-
-            </form>
+          <button type="submit">add</button>
+      </form>
     </>
   );
 }
