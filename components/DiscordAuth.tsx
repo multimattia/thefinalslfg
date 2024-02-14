ebc1e39d1c3750562df6a3453f8636a2f09ca78e
maat <matt@mattchan.xyz>
2024-02-14 12:25:23 -0500

    Resolve discord auth merge...?

--- components/DiscordAuth.tsx
+++ components/DiscordAuth.tsx
@@ -1,79 +1,64 @@
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import Image from "next/image";
+import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
+import { headers, cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default async function DiscordAuth() {
-  "use server";
-  const cookieStore = cookies();
+export default async function AuthButton() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
 
-  const supabase = createClient(cookieStore);
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   const signOut = async () => {
-    "use server";
+    'use server'
 
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    await supabase.auth.signOut();
-    return redirect("/");
-  };
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    await supabase.auth.signOut()
+    return redirect('/login')
+  }
 
-  const signIn = async () => {
-    "use server";
-    const getURL = () => {
-      let url = "";
-      if (process?.env?.NEXT_PUBLIC_SITE_URL) {
-        url = `${process?.env?.NEXT_PUBLIC_SITE_URL}/auth/callback`;
-      } else if (process?.env?.NEXT_PUBLIC_VERCEL_URL) {
-        url = `${process?.env?.NEXT_PUBLIC_VERCEL_URL}/auth/callback`;
-      } else {
-        url = "http://localhost:3000/auth/callback";
-      }
-      // Make sure to include `https://` when not localhost.
-      url = url.includes("http") ? url : `https://${url}`;
-      // Make sure to include a trailing `/`.
-      url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-      return url;
-    };
+  const signUp = async (formData: FormData) => {
+    'use server'
 
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "discord",
+    // const origin = headers().get("origin");
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
       options: {
-        redirectTo: getURL(),
+        emailRedirectTo: `${origin}/auth/callback`,
       },
-    });
+    })
+
     if (error) {
-      console.error(error);
+      return redirect('/login?message=Could not authenticate user')
     }
-    return redirect(data.url!);
-  };
+
+    return redirect('/login?message=Check email to continue sign in process')
+  }
 
   return user ? (
     <div className="flex items-center gap-4">
+      Hey, {user.email}!
       <form action={signOut}>
-        <button className="rounded-md bg-btn-background px-4 py-2 font-sans no-underline hover:bg-btn-background-hover">
+        <button className="rounded-md bg-btn-background px-4 py-2 no-underline hover:bg-btn-background-hover">
           Logout
         </button>
       </form>
-      {user.user_metadata.full_name}
-      <Image
-        className="rounded-full"
-        src={user.user_metadata.picture}
-        width={35}
-        height={35}
-        alt="Picture of the current user taken from Discord"
-      />
     </div>
   ) : (
-    <form action={signIn}>
-      <button className="rounded-md bg-btn-background px-4 py-2 font-sans no-underline hover:bg-btn-background-hover">
-        Sign in with Discord
-      </button>
-    </form>
-  );
+    <Link
+      href="/loginWithDiscord"
+      className="flex rounded-md bg-btn-background px-3 py-2 no-underline hover:bg-btn-background-hover"
+    >
+      Login
+    </Link>
+  )
 }
