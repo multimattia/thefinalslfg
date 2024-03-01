db4486cb178eb7b4a3819c95c5b7ce047bbe2bb0
maat <matt@mattchan.xyz>
2024-03-01 11:07:22 -0500

    Add new type MainSchema

--- lib/formschema.ts
+++ lib/formschema.ts
@@ -20,6 +20,8 @@ export const REGIONS = [
   "South America",
 ] as const;
 
+export const CLASSES = ["Light", "Medium", "Heavy"] as const;
+
 export const PLATFORMS = ["PlayStation", "Steam", "Xbox"] as const;
 
 export const multiSelectify = (options: string[]) => {
@@ -50,7 +52,29 @@ export const FormDataSchema = z.object({
   platform: z.enum(PLATFORMS).array(),
 });
 
+export const MainSchema = z.object({
+  embarkUsername: z.string().regex(new RegExp("^.{3,32}#[0-9]{4}$"), {
+    message: "Username must follow the Embark ID format (username#1234)",
+  }),
+  discordUsername: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Username cannot be more than 30 characters.",
+    }),
+  notes: z.string().min(2, {
+    message: "Note must be at least 2 characters.",
+  }),
+  rank: z.enum(RANKS),
+  region: z.enum(REGIONS),
+  platform: z.enum(PLATFORMS).array(),
+  class: z.enum(CLASSES).array(),
+});
+
 type Listing = z.infer<typeof FormDataSchema>;
+export type Record = z.infer<typeof MainSchema>;
 
 export const columns: ColumnDef<Listing>[] = [
   {
